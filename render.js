7e184992b82de4febbb255efd21061a9c65b4300
PatrykBan <147255233+PatrykBan@users.noreply.github.com>
2023-11-06 01:11:29 +0100

    Add files via upload

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    fetch("https://run.mocky.io/v3/5d88d742-4ccf-4638-b844-ab312c76300e").then(function (response) {
+    fetch("https://run.mocky.io/v3/b1586ff0-10a8-436a-aa35-70fe591914d0").then(function (response) {
         return response.json()
     }).then(function (object) {
         const posts = object.posts
@@ -15,6 +15,14 @@ window.onload = () => {
                                 <img src="${posts[i].image}">
                             </div>
                             <div class="display">
+                                <div class="top">
+                                    <div class="stopnie">
+                                        <p class="stopienos">- ${posts[i].stopnie} +</p>
+                                    </div>
+                                    <div class="time">
+                                    <img src="zegar.jpg" class="zegarson"> 7g, 30min
+                                    </div>
+                                </div>
                                 <div class="post_title">
                                     <h2>${posts[i].title}</h2>
                                 </div>
@@ -24,9 +32,16 @@ window.onload = () => {
                                 <div class="post_description">
                                     <p class="p-description">${posts[i].description}</p>
                                 </div>
-                                <div class="user_info">
-                                    <img src="${posts[i].userImg}" class="user_img">
-                                    <p class="p-user_info">  ${posts[i].userName}</p>
+                                <div class="bottom">
+                                    <div class="p-user_info">
+                                        <img src="${posts[i].userImg}" class="user_img">
+                                        <p class="p-user_info">  ${posts[i].userName}</p>
+                                    </div>
+                                    <div class="right side">
+                                        <a class="ulubione_button" href="#"><img src="ulubione.png"></a>
+                                        <a class="ulubione_button" href="#"><img src="commnets.png"></a>
+                                        <button class="okazje_button">Idź do okazji <img src="okazje.png"></button>
+                                    </div
                                 </div>
                             </div>
                         </div>
